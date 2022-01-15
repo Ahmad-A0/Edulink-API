cd94bcba49d79970b75578d71184b0218a293887
aa0 <71089234+Ahmad-A0@users.noreply.github.com>
2022-01-15 19:00:05 +0000

    add docs

--- .scripts/test.js
+++ .scripts/test.js
@@ -1,42 +1,28 @@
 import dotenv from 'dotenv';
 import path from 'path';
-import Edulink_API from './index.js';
-
+import Edulink_API from '../build/index.js';
 //prettier-ignore
-const isGetter = (x: any, name: PropertyKey) => (Object.getOwnPropertyDescriptor(x, name) || {}).get;
+const isGetter = (x, name) => (Object.getOwnPropertyDescriptor(x, name) || {}).get;
 //prettier-ignore
-const isFunction = (x: { [x: string]: any }, name: string) => typeof x[name] === 'function';
+const isFunction = (x, name) => typeof x[name] === 'function';
 //prettier-ignore
-const deepFunctions = (x: Object): any => x && x !== Object.prototype && Object.getOwnPropertyNames(x) .filter((name) => isGetter(x, name) || isFunction(x, name)) .concat(deepFunctions(Object.getPrototypeOf(x)) || []);
+const deepFunctions = (x) => x && x !== Object.prototype && Object.getOwnPropertyNames(x).filter((name) => isGetter(x, name) || isFunction(x, name)).concat(deepFunctions(Object.getPrototypeOf(x)) || []);
 //prettier-ignore
-const distinctDeepFunctions = (x: any): string[] => Array.from(new Set(deepFunctions(x)));
+const distinctDeepFunctions = (x) => Array.from(new Set(deepFunctions(x)));
 //prettier-ignore
-const userFunctions = (x: any): any => distinctDeepFunctions(x).filter( (name: any) => name !== 'constructor' && !~name.indexOf('__'));
-
+const userFunctions = (x) => distinctDeepFunctions(x).filter((name) => name !== 'constructor' && !~name.indexOf('__'));
 dotenv.config({
   path: path.join(__dirname, '../.env'),
 });
-
-declare const process: {
-  env: {
-    EDULINK_SCHOOL_ID: string;
-    EDULINK_USERNAME: string;
-    EDULINK_PASSWORD: string;
-  };
-};
-
-const skipList: string[] = ['TeacherPhotos'];
-
+const skipList = ['TeacherPhotos'];
 let [passed, failed] = [0, 0];
-
 (async () => {
-  const edulink: any = new Edulink_API();
+  const edulink = new Edulink_API();
   edulink.Authenticate({
     school_code: process.env.EDULINK_SCHOOL_ID,
     username: process.env.EDULINK_USERNAME,
     password: process.env.EDULINK_PASSWORD,
   });
-
   for (const property of userFunctions(edulink)) {
     if (
       property[0].toLowerCase() === property[0] ||
@@ -44,9 +30,7 @@ let [passed, failed] = [0, 0];
     ) {
       continue;
     }
-
     const response = await edulink[property]();
-
     if (response.result.success) {
       console.log(`${property}:`, 'passed');
       passed++;
@@ -55,9 +39,7 @@ let [passed, failed] = [0, 0];
       failed++;
     }
   }
-
   edulink.Authentication.Edulink_Logout();
-
   console.log('\n');
   console.log(`Passed: ${passed}`);
   console.log(`Failed: ${failed}`);
---
+++ docs/modules.md
@@ -0,0 +1,10 @@
+[edulink-api](README.md) / Modules
+
+# edulink-api
+
+## Table of contents
+
+### Modules
+
+- [Raw\_Edulink\_Response\_Types/Edulink\_Raw\_Response\_Types](modules/Raw_Edulink_Response_Types_Edulink_Raw_Response_Types.md)
+- [index](modules/index.md)
