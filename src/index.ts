352a8ccff9a251357aa4290d0e2065e9e14046e3
Ahmad-A0 <71089234+Ahmad-A0@users.noreply.github.com>
2022-02-13 14:01:10 +0000

    Create DO_NOT_PUBLISH

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Edulink_API from './API/API.js';
+import Edulink_Raw from './Raw_Edulink_Requests/Edulink_Raw.js';
+import * as Edulink_Raw_Response_Types from './Raw_Edulink_Response_Types/Edulink_Raw_Response_Types.js';
+
+export default Edulink_API;
+export { Edulink_API, Edulink_Raw, Edulink_Raw_Response_Types };
